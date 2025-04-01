0413cc307c6c66875006781bf896837063db5e1a
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-04-01 02:24:53 -0700

    cleanup

--- examples/with-ai-sdk/app/MyRuntimeProvider.tsx
+++ examples/with-ai-sdk/app/MyRuntimeProvider.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import { useChat } from "@ai-sdk/react";
 import { AssistantRuntimeProvider } from "@assistant-ui/react";
 import { useVercelUseChatRuntime } from "@assistant-ui/react-ai-sdk";
-import { useChat } from "ai/react";
 
 export function MyRuntimeProvider({
   children,
@@ -11,16 +11,9 @@ export function MyRuntimeProvider({
 }>) {
   const chat = useChat({
     api: "/api/chat",
-    onToolCall: ({ toolCall }) => {
-      console.log("test :", toolCall);
-    },
-    onFinish: (msg) => {
-      console.log("msg: ", msg);
-    },
     maxSteps: 5,
   });
 
-  console.log("chat: ", chat.messages);
   const runtime = useVercelUseChatRuntime(chat);
 
   return (
