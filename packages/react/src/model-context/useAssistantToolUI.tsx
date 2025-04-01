fb5c77e93b8d3dcc54a3281f6a335f204fe469c0
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-03-31 22:29:33 -0700

    tool done

--- packages/react/src/model-context/useAssistantToolUI.tsx
+++ packages/react/src/model-context/useAssistantToolUI.tsx
@@ -12,23 +12,36 @@ export type AssistantToolUIProps<TArgs, TResult> = {
 
 type Parameters = z.ZodTypeAny;
 type InferParameters<P extends Parameters> = z.infer<P>;
-type ToolTest<P extends Parameters = any, Res = any> = {
-  parameters: P;
-  description?: string;
-  client: (args: InferParameters<P>) => Promise<Res> | Res;
-  render: (args: Res) => React.ReactNode;
-};
+type ToolTest<P extends Parameters = any, Res = any> =
+  | {
+      parameters: P;
+      description?: string;
+      client: (args: InferParameters<P>) => PromiseLike<Res> | Res;
+      render?: (args: Res) => React.ReactNode;
+    }
+  | {
+      parameters: P;
+      description?: string;
+      server: (args: InferParameters<P>) => PromiseLike<Res> | Res;
+      render?: (args: Res) => React.ReactNode;
+    };
 
 const auiTool = <T extends Parameters, Res>(
-  a: ToolTest<T, Res> & {
-    client: (args: InferParameters<T>) => Res;
-  },
+  a:
+    | (ToolTest<T, Res> & {
+        client: (args: InferParameters<T>) => Res;
+        render?: (args: Res) => React.ReactNode;
+      })
+    | {
+        server: (args: InferParameters<T>) => Res;
+        render?: (args: Res) => React.ReactNode;
+      },
 ) => a;
 
 auiTool({
   parameters: z.string(),
+  // server: (a) => parseInt(a),
   client: (a) => parseInt(a),
-  render: (b) => <div>{b}</div>,
 });
 
 export const useAssistantToolUI = (
